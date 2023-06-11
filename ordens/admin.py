41358790d7d7f6786c2f416e8e06cc3d8f5ce654
MakalisterAndrade <makalister.andrade@outlook.com>
2023-06-11 08:32:57 -0400

    add de search_fields e search_help_text para ordens, forms e vies de pessoas

--- ordens/admin.py
+++ ordens/admin.py
@@ -6,23 +6,32 @@ from ordens.models import Item, Servico, Peca, Veiculo, OrdemDeServico
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['descricao', 'preco']
-
+    search_fields = ['descricao']
+    search_help_text = "Digite o nome do item para pesquisar"
 
 @admin.register(Servico)
 class ServicoAdmin(admin.ModelAdmin):
     list_display = ['item']
+    search_fields = ['item__descricao']
+    search_help_text = "Digite a descrição do item para pesquisar"
 
 
 @admin.register(Peca)
 class PecaAdmin(admin.ModelAdmin):
     list_display = ['item']
+    search_fields = ['item__descricao']
+    search_help_text = "Digite a descrição do item para pesquisar"
 
 
 @admin.register(Veiculo)
 class VeiculoAdmin(admin.ModelAdmin):
     list_display = ['placa', 'descricao', 'cliente', 'equipe']
+    search_fields = ['placa', 'descricao', 'cliente__pessoa__nome']
+    search_help_text = "Digite a placa, descrição do veículo ou nome do cliente para pesquisar"
 
 
 @admin.register(OrdemDeServico)
 class OrdemDeServicoAdmin(admin.ModelAdmin):
     list_display = ['emitida', 'veiculo', 'entrega', 'total']
+    search_fields = ['veiculo__placa','emitida']
+    search_help_text = "Digite a placa do veículo ou data da ordem de serviço para pesquisar"
--- pessoas/views.py
+++ pessoas/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from pessoas.forms import EnderecoForm, PessoaForm, ClienteForm, MecanicoForm, EquipeForm
 
-# Create your views here.
+def create_endereco(request):
+    if request.method == 'POST':
+        form = EnderecoForm(request.POST)
+        if form.is_valid():
+            endereco = form.save()
+            return redirect('endereco_detail', pk=endereco.pk)
+    else:
+        form = EnderecoForm()
+    return render(request, 'pessoas/endereco_create.html', {'form': form})
+
+
+def create_pessoa(request):
+    if request.method == 'POST':
+        form = PessoaForm(request.POST)
+        if form.is_valid():
+            pessoa = form.save()
+            return redirect('pessoa_detail', pk=pessoa.pk)
+    else:
+        form = PessoaForm()
+    return render(request, 'pessoas/pessoa_create.html', {'form': form})
+
+
+def create_cliente(request):
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            cliente = form.save()
+            return redirect('cliente_detail', pk=cliente.pessoa.pk)
+    else:
+        form = ClienteForm()
+    return render(request, 'pessoas/cliente_create.html', {'form': form})
+
+
+def create_mecanico(request):
+    if request.method == 'POST':
+        form = MecanicoForm(request.POST)
+        if form.is_valid():
+            mecanico = form.save()
+            return redirect('mecanico_detail', pk=mecanico.pessoa.pk)
+    else:
+        form = MecanicoForm()
+    return render(request, 'pessoas/mecanico_create.html', {'form': form})
+
+
+def create_equipe(request):
+    if request.method == 'POST':
+        form = EquipeForm(request.POST)
+        if form.is_valid():
+            equipe = form.save()
+            return redirect('equipe_detail', pk=equipe.pk)
+    else:
+        form = EquipeForm()
+    return render(request, 'pessoas/equipe_create.html', {'form': form})
