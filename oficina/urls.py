4ad709391e231e5c71f251fe3bdf19977aa2b0e1
alexgirardello <alexandre.girardello@estudante.ifro.edu.br>
2023-06-12 16:18:18 -0400

    Index

--- oficina/urls.py
+++ oficina/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from oficina.views import IndexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
     path('', include('pessoas.urls')),
     path('', include('ordens.urls'))
 ]
