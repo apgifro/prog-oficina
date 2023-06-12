0ca2c7be2babcbf56190d9972767af9cfc8155d5
MakalisterAndrade <makalister.andrade@outlook.com>
2023-06-12 19:49:32 -0400

    add create, update, delete in veiculos, add choices nos estado ao cadastras clientes

--- ordens/urls.py
+++ ordens/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from ordens.views import PecasReadView, ServicosReadView, VeiculosReadView, OrdemReadView
+from ordens.views import PecasReadView, ServicosReadView, VeiculoCreateView, VeiculoDeleteView, VeiculosReadView, \
+    OrdemReadView, VeiculoUpdateView
 
 urlpatterns = [
     path("pecas/", PecasReadView.as_view(), name="pecas_list"),
     path("servicos/", ServicosReadView.as_view(), name="servicos_list"),
     path("veiculos/", VeiculosReadView.as_view(), name="veiculos_list"),
+    path('veiculos/cadastrar/', VeiculoCreateView.as_view(), name='cadastrar_veiculo'),
+    path('veiculos/<str:placa>/update/', VeiculoUpdateView.as_view(), name='veiculo_update'),
+    path('veiculos/<str:placa>/delete/', VeiculoDeleteView.as_view(), name='veiculo_delete'),
     path("ordens/", OrdemReadView.as_view(), name="ordem_list"),
 ]
